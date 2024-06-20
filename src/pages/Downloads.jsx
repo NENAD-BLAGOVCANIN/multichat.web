2cfad46fd07476e05b14a69151436c2703793421
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-06-21 01:44:40 +0200

    updates

--- src/pages/Downloads.jsx
+++ src/pages/Downloads.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faApple, faWindows } from '@fortawesome/free-brands-svg-icons'
 import { useTranslation } from 'react-i18next';
-import { faDownload } from '@fortawesome/free-solid-svg-icons'
+import { faCheck, faDownload, faX } from '@fortawesome/free-solid-svg-icons'
 
 function Downloads() {
 
@@ -11,11 +11,11 @@ function Downloads() {
   return (
     <div className='container py-5 px-5'>
 
-      <h2>{t('downloads.downloads')}</h2>
+      <h2>Free</h2>
 
       <div className="row">
         <div className="col-md-6 py-4">
-          <div className='card border-0 shadow-sm rounded-lg'>
+          <div className='card border-0 shadow-sm rounded-0'>
             <div className='card-body'>
               <div className='d-flex justify-content-center align-items-center h-100'>
                 <h4 className='mt-3'>
@@ -35,11 +35,38 @@ function Downloads() {
                   <a className='text-decoration-none color-text small'>{t('downloads.system_reqs')}</a>
                 </div>
               </div>
+
+              <hr />
+
+              <div className='px-5'>
+                <div className='d-flex flex-column justify-content-between px-5'>
+                  <div className='d-flex align-items-center h-100 justify-content-between py-1'>
+                    <span>Unlimited Tabs</span>
+                    <FontAwesomeIcon icon={faX} className='text-danger' />
+                  </div>
+                  <div className='d-flex align-items-center h-100 justify-content-between py-1'>
+                    <span>Unlimited Sessions</span>
+                    <FontAwesomeIcon icon={faX} className='text-danger' />
+                  </div>
+                  <div className='d-flex align-items-center h-100 justify-content-between py-1'>
+                    <span>Automatic Updates</span>
+                    <FontAwesomeIcon icon={faCheck} className='text-success' />
+                  </div>
+                  <div className='d-flex align-items-center h-100 justify-content-between py-1'>
+                    <span>24/7 Customer Support</span>
+                    <FontAwesomeIcon icon={faCheck} className='text-success' />
+                  </div>
+
+                </div>
+              </div>
+
+
+
             </div>
           </div>
         </div>
         <div className="col-md-6 py-4">
-          <div className='card border-0 shadow-sm rounded-lg'>
+          <div className='card border-0 shadow-sm rounded-0'>
             <div className='card-body'>
               <div className='d-flex justify-content-center align-items-center h-100'>
                 <h4 className='mt-3'>
@@ -59,6 +86,136 @@ function Downloads() {
                   <a className='text-decoration-none color-text small'>{t('downloads.system_reqs')}</a>
                 </div>
               </div>
+
+              <hr />
+
+              <div className='px-5'>
+                <div className='d-flex flex-column justify-content-between px-5'>
+                  <div className='d-flex align-items-center h-100 justify-content-between py-1'>
+                    <span>Unlimited Tabs</span>
+                    <FontAwesomeIcon icon={faX} className='text-danger' />
+                  </div>
+                  <div className='d-flex align-items-center h-100 justify-content-between py-1'>
+                    <span>Unlimited Sessions</span>
+                    <FontAwesomeIcon icon={faX} className='text-danger' />
+                  </div>
+                  <div className='d-flex align-items-center h-100 justify-content-between py-1'>
+                    <span>Automatic Updates</span>
+                    <FontAwesomeIcon icon={faCheck} className='text-success' />
+                  </div>
+                  <div className='d-flex align-items-center h-100 justify-content-between py-1'>
+                    <span>24/7 Customer Support</span>
+                    <FontAwesomeIcon icon={faCheck} className='text-success' />
+                  </div>
+
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <h2 className='mt-3'>Premium</h2>
+
+      <div className="row">
+        <div className="col-md-6 py-4">
+          <div className='card border-0 shadow-sm rounded-0'>
+            <div className='card-body'>
+              <div className='d-flex justify-content-center align-items-center h-100'>
+                <h4 className='mt-3'>
+                  <FontAwesomeIcon icon={faWindows} className='pe-2' />
+                  Windows
+                </h4>
+              </div>
+
+              <div className='h-100 d-flex justify-content-center align-items-center pt-3'>
+                <div className='p-3'>
+                  <a href='https://multichat-storage.netlify.app/setup.exe' className='btn btn-primary w-fit rounded-lg py-3 px-4'>
+                    <FontAwesomeIcon className='pe-2' icon={faDownload} />
+                    {t('home.hero_section.download_for_windows')}
+                  </a>
+                </div>
+                <div className='p-3'>
+                  <a className='text-decoration-none color-text small'>{t('downloads.system_reqs')}</a>
+                </div>
+              </div>
+
+              <hr />
+
+              <div className='px-5'>
+                <div className='d-flex flex-column justify-content-between px-5'>
+                  <div className='d-flex align-items-center h-100 justify-content-between py-1'>
+                    <span>Unlimited Tabs</span>
+                    <FontAwesomeIcon icon={faCheck} className='text-success' />
+                  </div>
+                  <div className='d-flex align-items-center h-100 justify-content-between py-1'>
+                    <span>Unlimited Sessions</span>
+                    <FontAwesomeIcon icon={faCheck} className='text-success' />
+                  </div>
+                  <div className='d-flex align-items-center h-100 justify-content-between py-1'>
+                    <span>Automatic Updates</span>
+                    <FontAwesomeIcon icon={faCheck} className='text-success' />
+                  </div>
+                  <div className='d-flex align-items-center h-100 justify-content-between py-1'>
+                    <span>24/7 Customer Support</span>
+                    <FontAwesomeIcon icon={faCheck} className='text-success' />
+                  </div>
+
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6 py-4">
+          <div className='card border-0 shadow-sm rounded-0'>
+            <div className='card-body'>
+              <div className='d-flex justify-content-center align-items-center h-100'>
+                <h4 className='mt-3'>
+                  <FontAwesomeIcon icon={faApple} className='pe-2' />
+                  MacOS
+                </h4>
+              </div>
+
+              <div className='h-100 d-flex justify-content-center align-items-center pt-3'>
+                <div className='p-3'>
+                  <a href='https://multichat-storage.netlify.app/setup.exe' className='btn btn-primary w-fit rounded-lg py-3 px-4'>
+                    <FontAwesomeIcon className='pe-2' icon={faDownload} />
+                    {t('home.hero_section.download_for_windows')}
+                  </a>
+                </div>
+                <div className='p-3'>
+                  <a className='text-decoration-none color-text small'>{t('downloads.system_reqs')}</a>
+                </div>
+              </div>
+
+              <hr />
+
+              <div className='px-5'>
+                <div className='d-flex flex-column justify-content-between px-5'>
+                  <div className='d-flex align-items-center h-100 justify-content-between py-1'>
+                    <span>Unlimited Tabs</span>
+                    <FontAwesomeIcon icon={faCheck} className='text-success' />
+                  </div>
+                  <div className='d-flex align-items-center h-100 justify-content-between py-1'>
+                    <span>Unlimited Sessions</span>
+                    <FontAwesomeIcon icon={faCheck} className='text-success' />
+                  </div>
+                  <div className='d-flex align-items-center h-100 justify-content-between py-1'>
+                    <span>Automatic Updates</span>
+                    <FontAwesomeIcon icon={faCheck} className='text-success' />
+                  </div>
+                  <div className='d-flex align-items-center h-100 justify-content-between py-1'>
+                    <span>24/7 Customer Support</span>
+                    <FontAwesomeIcon icon={faCheck} className='text-success' />
+                  </div>
+
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
