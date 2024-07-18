b052e296953e8e3968d055018c3730a2db9191b1
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-07-18 13:32:37 +0200

    updates

--- src/pages/account/Account.jsx
+++ src/pages/account/Account.jsx
@@ -1,25 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { getUserInfo } from '../../api/user';
+import { faCamera, faUser } from '@fortawesome/free-solid-svg-icons';
 
 function Account() {
+    const [userInfo, setUserInfo] = useState([]);
+
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            try {
+                const fetchedUserInfo = await getUserInfo();
+                setUserInfo(fetchedUserInfo);
+            } catch (error) {
+                console.error('Error fetching :', error);
+            }
+        };
+
+        fetchUserInfo();
+    }, []);
+
+    function formatDate(timestamp) {
+        const date = new Date(timestamp);
+        const options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            timeZoneName: 'short',
+        };
+        return date.toLocaleDateString(undefined, options);
+    }
+
+
     return (
-        <div className="container px-5">
+        <div className='container px-5 py-5'>
 
-            <h3 className="mt-5 pb-5 bold">My Account</h3>
+            <h2 className='bold mb-5'>My Account</h2>
 
-            <p className="text-muted">Name: NENAD BLAGOVCANIN</p>
-            <p className="text-muted">Email: nenadblagovcanin2@gmail.com</p>
+
+            <p className="text-muted">Name: {userInfo.name}</p>
+            <p className="text-muted">Email: {userInfo.email}</p>
             <p className="text-muted">
-                Profile Created At: 12 April
+                Profile Created At: {formatDate(userInfo.date_joined)}
             </p>
             <p className="text-muted">
                 Email Verified:{" "}
                 <span className="badge bagde-primary bg-danger">No</span>
-            </p>
-            <p className="text-muted m-0">
-                Subscription:
-                <span className="badge bagde-primary bg-success">
-                    Free
-                </span>
             </p>
 
         </div>
