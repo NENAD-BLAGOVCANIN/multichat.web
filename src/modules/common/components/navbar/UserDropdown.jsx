a8812709a2cbf24e1fa736351250a0fa7aac3d87
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-07-26 23:37:09 +0200

    updates

--- src/modules/common/components/navbar/UserDropdown.jsx
+++ src/modules/common/components/navbar/UserDropdown.jsx
@@ -1,27 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChartSimple, faUser, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { BoxArrowDownRight, PersonCircle, UiChecksGrid } from 'react-bootstrap-icons'
+import { getUserInfo } from '../../api/user'
 
 function UserDropdown() {
 
     const { t } = useTranslation();
+    const [userInfo, setUserInfo] = useState([]);
+
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            try {
+                const fetchedUserInfo = await getUserInfo();
+                setUserInfo(fetchedUserInfo);
+            } catch (error) {
+                console.error('Error fetching :', error);
+            }
+        };
+
+        fetchUserInfo();
+    }, []);
+
 
     return (
         <div>
             <div className="dropdown-menu dropdown-menu-left shadow-lg rounded show active" id='accountDropdown' aria-labelledby="accountDropdown"
                 style={{ position: 'absolute', width: 300 }}>
 
-                <li className='nav-item px-2 rounded'>
-                    <Link to="/admin/dashboard" className='nav-link py-1'>
-                        <UiChecksGrid />
-                        <span className='ps-3 medium'>Dashboard</span>
-                    </Link>
-                </li>
+                {
+                    userInfo.is_admin && (
+                        <>
+                            <li className='nav-item px-2 rounded'>
+                                <Link to="/admin/dashboard" className='nav-link py-1'>
+                                    <UiChecksGrid />
+                                    <span className='ps-3 medium'>Dashboard</span>
+                                </Link>
+                            </li>
 
-                <hr className='text-secondary' />
+                            <hr className='text-secondary' />
+                        </>
+                    )
+                }
+
 
                 <li className='nav-item px-2 rounded'>
                     <Link to="/account" className='nav-link py-1'>
