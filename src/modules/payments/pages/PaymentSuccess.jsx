f209ed32ee6d51512e70bff9aeef748662d776db
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-08-25 12:26:24 +0200

    updates

--- src/modules/payments/pages/PaymentSuccess.jsx
+++ src/modules/payments/pages/PaymentSuccess.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import { ShieldFillCheck } from 'react-bootstrap-icons';
 
 function PaymentSuccess() {
+
+    const formatDate = (date) => {
+        const options = { year: 'numeric', month: 'short', day: 'numeric' };
+        return date.toLocaleDateString('en-US', options);
+    };
+
+    const today = new Date();
+
     return (
         <div style={{ minHeight: '90vh' }} className='container w-100 d-flex align-items-center justify-content-center bg-white'>
             <div class="row">
@@ -14,8 +22,8 @@ function PaymentSuccess() {
                             <h2 class="bold mb-3">Thank you for your order!</h2>
                             <p class="card-text mb-1">You will receive an email with details of your subscription and a monthly reminder for next payments.</p>
                             <p class="card-text mb-1">All necessary information about the subscription, will be sent to your email</p>
-                            <p class="card-text text-order badge bg-success my-3">Your payment # is: <b>00000123</b></p>
-                            <p class="card-text mb-0">Order date: 14 Nov 2021</p>
+                            <p class="card-text text-order badge bg-success my-3">Your payment # is: <b>00000523</b></p>
+                            <p class="card-text mb-0">Order date: {formatDate(today)}</p>
                         </div>
                     </div>
                 </div>
